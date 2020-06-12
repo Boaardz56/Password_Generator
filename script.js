edb4c297c9c049f17a83a6ea19a9a9fea49c657f
Boaardz56 <jackie.lopez.56@gmail.com>
2020-06-12 10:14:47 -0400

    1st commit to js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//Create array of variables
+var passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var passNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var passSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "_"];
+var passCriteria = [];
+
+//builing password
+function generatePassword() {
+
+  //prompt to ask how many character user wants
+  var numberOfChar = prompt("How many characters do you want?");
+  //set a min and max range
+  if (numberOfChar > 7 && numberOfChar < 25)
+  //prompt to ask what other values they want
+  var confirmUpper = confirm("Do you want uppercase letters?");
+  var confirmLower = confirm("Do you want lowercase letters?");
+  var confirmNumbers = confirm("Do you want numbers?");
+  var confirmSpecial = confirm("Do you want special characters?");
+
+  //add in conditional statements
+  if (confirmUpper) {
+    passCriteria.concat(passUpperChar);
+  }
+  else {
+    return false
+  }
+
+  if (confirmLower) {
+    passCriteria.concat(passLowerChar);
+  }
+  else {
+    return false
+  }
+
+  if (confirmNumbers) {
+    passCriteria.concat(passNumbers);
+  }
+  else {
+    return false
+  }
+
+  if (confirmSpecial) {
+    passCriteria.concat(passSpecial);
+  }
+  else {
+    return false
+  }
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
