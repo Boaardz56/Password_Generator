29ff17e2cc0927f3bffa36953243be89decad054
Boaardz56 <jackie.lopez.56@gmail.com>
2020-06-12 18:17:12 -0400

    4th commit to js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //getting into button
 var generateBtn = document.querySelector("#generate");
+
 //Create array of variables for globals
 var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
@@ -8,55 +9,71 @@ var passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 var passLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var passNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var passSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "_"];
+var totalPassChar = ["passUpperChar" + "passLowerChar" + "passSpecial" + "passNumbers"];
+var randomSet = new Random();
 var passCriteria = [];
+
+//initialize random functions array
+let password = {};
+let randomChar = "";
+// let totalPassword;
 
 //builing password
 function generatePassword() {
 
-  //prompt to ask how many character user wants
-  var numberOfChar = prompt("How many characters do you want?");
-  //set a min and max range
-  if (numberOfChar > 7 && numberOfChar < 25) {
+  //prompt to ask how many character user wants for min and max range
+var numberOfChar = prompt("Enter a number of character between 8-128");
+
+ if (numberOfChar <=8 && numberOfChar <=128) {
+   alert("Password length must be between 8-128 characters");
+ }
+   
   //prompt to ask what other values they want
     var confirmUpper = confirm("Do you want uppercase letters?");
     var confirmLower = confirm("Do you want lowercase letters?");
     var confirmNumbers = confirm("Do you want numbers?");
     var confirmSpecial = confirm("Do you want special characters?");
 
-    //add in conditional statements
-    if (confirmUpper) {
-      passCriteria.concat(passUpperChar);
+    console.log("All questions asked");
+  // define generators
+ 
+
+
+  var upperCaseGenerator
+
+
+   // add in conditional statements
+   if (confirmUpper === true) {
+      var upper = upperCaseGenerator[]
+      totalPassword += passCriteria.concat(passUpperChar);
      }
-     else {
-        return false
-      }
 
-    if (confirmLower) {
-      passCriteria.concat(passLowerChar);
+    if (confirmLower === true) {
+      var lower = lowerCaseGenerator[]
+      totalPassword += passCriteria.concat(passLowerChar);
      }
-     else {
-        return false
-      }
 
-    if (confirmNumbers) {
-     passCriteria.concat(passNumbers);
+    if (confirmNumbers === true) {
+      var number = numberGenerator()
+      totalPassword += passCriteria.concat(passNumbers);
     }
-    else {
-       return false
+    
+    if (confirmSpecial === true) {
+      var symbol = symbolGenerator()
+       totalPassword += passCriteria.concat(passSpecial);
     }
 
-    if (confirmSpecial) {
-       passCriteria.concat(passSpecial);
-    }
+    console.log("Password so far:" + totalPassword);
+    
     else {
+
       return false
     }
+ 
   }
-   else {
-     alert("Must be between 8 and 128 characters")
-   }
-}
 
+  
+  //return random char; 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -67,4 +84,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
