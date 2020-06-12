5c0a96e9a1d4f7b3d2e5d76fea3285905f9c374a
Boaardz56 <jackie.lopez.56@gmail.com>
2020-06-12 10:23:17 -0400

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//Create array of variables
+//getting into button
+var generateBtn = document.querySelector("#generate");
+//Create array of variables for globals
+var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*-=_";
 var passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var passLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var passNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -11,46 +16,46 @@ function generatePassword() {
   //prompt to ask how many character user wants
   var numberOfChar = prompt("How many characters do you want?");
   //set a min and max range
-  if (numberOfChar > 7 && numberOfChar < 25)
+  if (numberOfChar > 7 && numberOfChar < 25) {
   //prompt to ask what other values they want
-  var confirmUpper = confirm("Do you want uppercase letters?");
-  var confirmLower = confirm("Do you want lowercase letters?");
-  var confirmNumbers = confirm("Do you want numbers?");
-  var confirmSpecial = confirm("Do you want special characters?");
+    var confirmUpper = confirm("Do you want uppercase letters?");
+    var confirmLower = confirm("Do you want lowercase letters?");
+    var confirmNumbers = confirm("Do you want numbers?");
+    var confirmSpecial = confirm("Do you want special characters?");
 
-  //add in conditional statements
-  if (confirmUpper) {
-    passCriteria.concat(passUpperChar);
-  }
-  else {
-    return false
-  }
+    //add in conditional statements
+    if (confirmUpper) {
+      passCriteria.concat(passUpperChar);
+     }
+     else {
+        return false
+      }
 
-  if (confirmLower) {
-    passCriteria.concat(passLowerChar);
-  }
-  else {
-    return false
-  }
+    if (confirmLower) {
+      passCriteria.concat(passLowerChar);
+     }
+     else {
+        return false
+      }
 
-  if (confirmNumbers) {
-    passCriteria.concat(passNumbers);
-  }
-  else {
-    return false
-  }
+    if (confirmNumbers) {
+     passCriteria.concat(passNumbers);
+    }
+    else {
+       return false
+    }
 
-  if (confirmSpecial) {
-    passCriteria.concat(passSpecial);
+    if (confirmSpecial) {
+       passCriteria.concat(passSpecial);
+    }
+    else {
+      return false
+    }
   }
-  else {
-    return false
-  }
+   else {
+     alert("Must be between 8 and 128 characters")
+   }
 }
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
